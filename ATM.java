b40ec280b6b8299a48a3dbc9e0cbebf5bdd83eab
Aain337 <www.aatikayasin@gmail.com>
2025-07-19 10:37:57 +0500

    ATM code

--- ATM.java
+++ ATM.java
@@ -1,24 +1,41 @@
 import java.util.*;
  public class ATM{
-
+   //*********************** Card method *****************************
    public Card(int card){
       int CARD = 12345;
         if(card==CARD){
-           atm();
+           Pin();
         }
         else {
-         System.out.println("Card declined please try again");
+         System.out.println("Card declined please try again ");
            Card();
         }
    }
-
-   public atm(){
-      
+   //*************************** Pin method ************************
+   public Pin(){
+      int PIN = 4567;
+      System.out.println("Please enter the PIN ");
+      int pin = sc.nextInt();
+      if(pin == PIN){
+         Atm();
+      }
+      else{
+         System.out.println("Incorrect Pin please try again ");
+         Pin();
+      }
+   }
+   //******************************* Atm method ***********************
+   public Atm(){
+      System.out.println("Welcome to ATM service \n please select the option ");
+      While(True){
+         System.out.println("1.Check Balance");
+         System.out.println("2.Widthdraw money");
+      }
     }
     public static void main(String[] args){
       ATM obj = new ATM();
       Scanner sc = new Scanner(System.in);
-      System.out.println("Please enter your Card");
+      System.out.println("Please insert your Card ");
       int card = sc.nextInt();
 
     }
