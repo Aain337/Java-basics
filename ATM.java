edd829151e99ea9e4e30122b1180e939086be07e
Aain337 <www.aatikayasin@gmail.com>
2025-07-19 10:23:59 +0500

    ATM simple code

--- ATM.java
+++ ATM.java
@@ -1,9 +1,25 @@
 import java.util.*;
  public class ATM{
-    public atm(){
-        
+
+   public Card(int card){
+      int CARD = 12345;
+        if(card==CARD){
+           atm();
+        }
+        else {
+         System.out.println("Card declined please try again");
+           Card();
+        }
+   }
+
+   public atm(){
+      
     }
     public static void main(String[] args){
+      ATM obj = new ATM();
+      Scanner sc = new Scanner(System.in);
+      System.out.println("Please enter your Card");
+      int card = sc.nextInt();
 
     }
  }
