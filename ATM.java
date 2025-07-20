1ecd9b5da8d355f65147ca74bacb94254286036e
Aain337 <www.aatikayasin@gmail.com>
2025-07-20 18:09:30 +0500

    ATM code

--- ATM.java
+++ ATM.java
@@ -1,18 +1,21 @@
 import java.util.*;
  public class ATM{
+
    //*********************** Card method *****************************
-   public Card(int card){
+   public void Card(int card){
+      Scanner sc = new Scanner(System.in);
       int CARD = 12345;
         if(card==CARD){
            Pin();
         }
         else {
          System.out.println("Card declined please try again ");
-           Card();
+           //System.out.println(Card()); 
         }
    }
-   //*************************** Pin method ************************
-   public Pin(){
+   //*************************** Pin method **************************
+   public void Pin(){
+      Scanner sc = new Scanner(System.in);
       int PIN = 4567;
       System.out.println("Please enter the PIN ");
       int pin = sc.nextInt();
@@ -25,20 +28,22 @@ import java.util.*;
       }
    }
    //******************************* Atm method ***********************
-   public Atm(){
+   public void Atm(){
+      Scanner sc = new Scanner(System.in);
       System.out.println("Welcome to ATM service \n please select the option ");
-      While(True){
-         System.out.println("1.Check Balance");
-         System.out.println("2.Widthdraw Money");
-         System.out.println("3.Transfer Money");
-         System.out.println("Deposit Money");
-      }
+    //   while(true){
+    //     System.out.println("1.Check Balance");
+    //     System.out.println("2.Widthdraw Money");
+    //     System.out.println("3.Transfer Money");
+     //    System.out.println("Deposit Money");
+      //}
     }
-    public static void main(String[] args){
+    //************************* main method **************************
+   public static void main(String[] args){
       ATM obj = new ATM();
       Scanner sc = new Scanner(System.in);
       System.out.println("Please insert your Card ");
       int card = sc.nextInt();
-
+      obj.Card(card);
     }
  }
