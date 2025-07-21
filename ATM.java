c82222e863c672fc7ff707b2c1c2ec0a558ff0ae
Aain337 <www.aatikayasin@gmail.com>
2025-07-21 12:59:41 +0500

    ATM basic code

--- ATM.java
+++ ATM.java
@@ -37,44 +37,78 @@ import java.util.*;
       Scanner sc = new Scanner(System.in);
       System.out.println("Welcome to ATM service \n please select the option ");
       float Balance = 1000;
-    //   while(true){
-    //     System.out.println("1. Check Balance");
-    //     System.out.println("2. Widthdraw Money");
-    //     System.out.println("3. Transfer Money");
-     //    System.out.println("4. Deposit Money");
-           int input = sc.nextInt();
+       while(true)
+        {
+            System.out.println("1. Check Balance");
+            System.out.println("2. Widthdraw Money");
+            System.out.println("3. Transfer Money");
+            System.out.println("4. Deposit Money");
+            System.out.println("5. Exit ");
+            int input = sc.nextInt();
            switch(input)
-           {
-               case 1 :
-               {
-                     System.out.println("Your current balance is: " + Balance);
-                     break;
-               }
-               case 2 :
-               {
-                     System.out.println("Select the amount: ");
-                     float amount = sc.nextFloat();
-                     Balance = Balance-amount;
-                     System.out.println("Your current Balance is :" + Balance);
-                     break;
-               }
-               case 3 :
-               {
-                     System.out.println("Select the amount: ");
-                     float transfer = sc.nextFloat;
-                     System.out.println("Enter the number, you want to transfer : ");
-                     int receiver = sc.nextInt;
-                     Balance = Balance - transfer;
-                     System.out.println("The amount " + transfer + " is tranfered to " + receiver);
-                     System.out.println("Your remaining Balance is: " + Balance);
-                     break;
-               }
-               case 4 :
-               {
-                     System.out.println("Deposit the amount you want: ");
-                     break;
-               }
+            {
+                  case 1 :
+                  {
+                        System.out.println("Your current balance is: " + Balance);
+                        break;
+                  }
+                  case 2 :
+                  {
+                        System.out.println("Select the amount to withdraw: ");
+                        float amount = sc.nextFloat();
+                        if(amount > Balance)
+                        {
+                           System.out.println("The Balnce is not enough...\n Your balnce is: " + Balance);
+                           break;
+                        }
+                        else
+                        {
+                           Balance = Balance-amount;
+                           System.out.println("Your amount is withdrawn.");
+                           System.out.println("Your current Balance is :" + Balance);
+                        }
+                        break;
+                  }
+                  case 3 :
+                  {
+                        System.out.println("Select the amount to transfer: ");
+                        float transfer = sc.nextFloat();
+                         if(transfer > Balance)
+                        {
+                           System.out.println("The Balnce is not enough...\n Your balnce is: " + Balance);
+                           break;
+                        }
+                        else
+                        {
+                        System.out.println("Enter the number, you want to transfer to: ");
+                        int receiver = sc.nextInt();
+                        Balance = Balance - transfer;
+                        System.out.println("The amount " + transfer + " is tranfered to " + receiver);
+                        System.out.println("Your remaining Balance is: " + Balance);
+                        }
+                        break;
+                  }
+                  case 4 :
+                  {
+                        System.out.println("Deposit the amount you want: ");
+                        float deposit = sc.nextFloat();
+                        Balance = Balance + deposit;
+                        System.out.println("Your current balnce is: "+ Balance);
+                        break;
+                  }
+                  case 5 :
+                  {
+                     System.out.println("Thankyou for availing our services.");
+                     System.out.println("Please take your card.");
+                     return;
+                  }
+                  default :
+                  {
+                     System.out.println("Please select the right option....");
+                     return;
+                  }     
             }
+         }
    }
 
     //************************* main method **************************
